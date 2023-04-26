a005b5f4d5de19af7e2481641074949f21c6efcc
boazeb <boazeb@gmail.com>
2023-04-26 11:22:27 +0300

    remove homepage temporarily

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -29,46 +29,15 @@ function Homepage() {
                     <Row>
                         <Col xs={{span:12}} md={{span:12, offset:0}} lg={{span:8, offset:2}}>
                             <div className={'homepageH1'}>
-                                <div className={'homepageH1pretitle'}>Kosign</div>
+                                <div className={'homepageH1pretitle'}>Your Forever Vault</div>
                                 <div className={'actualTitle'}>
                                     <div>Cold Storage With Social Recovery</div>
                                 </div>
                             </div>
 
-                            <div className={'alert alert-info'} style={{textAlign:'center', marginTop:30}}>
-                                Kosign is free open source software. <a href='https://github.com/xxbtc/kosign-coldstorage'>Download and run it</a> from your offline device.
-                            </div>
 
-                            <div style={{marginTop:20, textAlign:'center'}}>
-                                <div style={{display:'flex',flexDirection:'row', justifyContent:'center', alignItems:'center', paddingTop:30}}>
-                                    <div>
-                                        <Button
-                                            variant={'primary'}
-                                            size={'lg'}
-                                            onClick={() => navigate('/create')}
-                                        >
-                                            Create vault
-                                        </Button>
-                                    </div>
-                                    <div style={{padding:4}}>
-                                        &nbsp; or &nbsp;
-                                    </div>
-                                    <div>
-                                        <Button
-                                            variant={'link'}
-                                            size={'lg'}
-                                            onClick={() => navigate('/recover')}
-                                        >
-                                            Recover backup
-                                        </Button>
-                                    </div>
-                                </div>
 
-                            </div>
-
-                            <div style={{marginTop:300, textAlign:'center'}}>
-
-                            </div>
+                            
                         </Col>
 
                     </Row>
