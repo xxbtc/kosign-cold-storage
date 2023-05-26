3eda7ffe5f71e1fda9d785ce845090e3d3d74a8c
boazeb <boazeb@gmail.com>
2023-05-26 23:43:10 +0300

    added twitter and blog

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -25,7 +25,12 @@ export default function Footer() {
                                 <li>
                                     <Link to={'/privacy'}>Privacy Policy</Link>
                                 </li>
-
+                                <li>
+                                    <a href={'https://twitter.com/kosign_xyz'} target={'_blank'}>@kosign_xyz</a>
+                                </li>
+                                <li>
+                                    <a href={'https://kosignxyz.medium.com'} target={'_blank'}>Blog</a>
+                                </li>
                             </ul>
                         </Col>
                     </Row>
